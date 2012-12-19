1786f103ee7bcd58a455ee4791d5c439e1ef528c
marcelogomesrp@gmail.com <marcelogomesrp@gmail.com@5caf940c-c058-c49b-f9da-0805abf854e0>
2012-12-19 18:12:43 +0000

    varias

---
+++ trunk/Neuropathologist/src/java/managedbean/GrupoMB.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbean;
+
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import model.Grupo;
+import service.GrupoService;
+
+/**
+ *
+ * @author marcelo
+ */
+@Named(value = "grupoMB")
+@SessionScoped
+public class GrupoMB implements Serializable{
+    @Inject
+    private GrupoService grupoService;
+    private Grupo grupo;
+
+    /**
+     * Creates a new instance of GrupoMB
+     */
+    public GrupoMB() {
+    }
+
+    @PostConstruct 
+    public void init(){
+        grupo = new Grupo();
+    }
+    public Grupo getGrupo() {
+//        if(grupo == null){
+//            grupo = new Grupo();
+//        }
+        return grupo;
+    }
+
+    public void setGrupo(Grupo grupo) {
+        this.grupo = grupo;
+    }
+    
+    public String salvar(){
+        grupoService.salvar(getGrupo());
+        grupo = new Grupo();
+        return null;
+    }
+
+    
+    
+}
--- trunk/Neuropathologist/src/java/model/Grupo.java
+++ trunk/Neuropathologist/src/java/model/Grupo.java
@@ -5,9 +5,7 @@
 package model;
 
 import java.io.Serializable;
-import java.util.Collection;
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,12 +13,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
@@ -40,9 +36,9 @@ public class Grupo implements Serializable {
     @Basic(optional = false)
     @Column(name = "id")
     private Long id;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 150)
+    @Basic(optional = true)
+    //@NotNull
+    //@Size(min = 1, max = 150)
     @Column(name = "nome")
     private String nome;
     //@OneToMany(cascade = CascadeType.ALL, mappedBy = "grupoId")
