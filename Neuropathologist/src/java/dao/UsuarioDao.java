e57945f2f68f277d8090bdc552dd86b52ee3ccd3
marcelogomesrp@gmail.com <marcelogomesrp@gmail.com@5caf940c-c058-c49b-f9da-0805abf854e0>
2012-12-19 13:30:53 +0000

    Criado o UsuarioDao

---
+++ Neuropathologist/src/java/dao/UsuarioDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import model.Usuario;
+
+/**
+ *
+ * @author marcelo
+ */
+public class UsuarioDao extends Dao<Usuario>{
+
+    public UsuarioDao() {
+        super(Usuario.class);
+    }
+    
+    
+}
